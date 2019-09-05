241506be11d176ed98361e830c7446addc860604
CDS2UNIVO15 <CDS2UNIVO15@CDS2UNIVO15>
2019-09-05 09:10:33 -0600

    PersonRepository Agregado

---
+++ People/People/People/PersonRepositoryCRUD.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using People.Models;
+
+namespace People
+{
+    class PersonRepositoryCRUD
+    {
+        SQLiteConnection conn;
+        public string StatusMessage { get; set; }
+        public PersonRepositoryCRUD(string dbPath)
+        {
+            //Creamos La Conexion
+            conn = new SQLiteConnection(dbPath);
+            conn.CreateTable<Person>();
+        }
+    }
+}
