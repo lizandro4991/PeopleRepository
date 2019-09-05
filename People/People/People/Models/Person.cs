e5f06e680a2ca264e46dc2907ebb37fd400ef796
CDS2UNIVO15 <CDS2UNIVO15@CDS2UNIVO15>
2019-09-05 08:38:57 -0600

    model class Person Agregado

---
+++ People/People/People/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace People.Models
+{
+    [Table("Tbl_People")]
+    public class Person
+    {
+        //Miembro Property
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public string name { get; set; }
+    }
+}
